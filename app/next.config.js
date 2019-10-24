f8286f8a81c747a6b8f43247c9a40ceddfc7471b
Moo-YewTsing <mooyewtsing@gmail.com>
2019-10-24 14:14:25 -0400

    app engine succeed

--- app/next.config.js
+++ app/next.config.js
@@ -1,11 +1,3 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config({
-    path: require('path').resolve(process.cwd(), '../.env')
-  })
-}
-
 module.exports = {
-  env: {
-    API_URL: process.env.API_URL || '/api'
-  }
-}
+  distDir: 'build'
+};
